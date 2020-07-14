683a585e195541a21e4800903c3f619d08b3c636
Shivam Kalra <shivamkalra98@gmail.com>
2020-07-14 06:46:25 +0530

    Add username to a post in views

--- 1/controllers/home_controller.js
+++ 1/controllers/home_controller.js
@@ -1,8 +1,20 @@
+const Post = require('../models/post')
 module.exports.home = function(req, res){
-    console.log(req.cookies);
+    //console.log(req.cookies);
     // res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts: posts,
+    //     });
+    // });
+    
+    //populate the user of each object
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Home",
+            posts: posts,
+        });
     });
 }
 
